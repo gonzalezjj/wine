8a929d02c1dc72addd8de2b218aa319e811422b7
Juan Jose Gonzalez <juanj.gh@gmail.com>
2016-03-03 17:04:40 +0100

    xinput: Fixed formatting of util.c

--- dlls/xinput1_3/util.c
+++ dlls/xinput1_3/util.c
@@ -31,14 +31,16 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(xinput);
 
-static XINPUTW_VALUE RangeFindCenter(XINPUTW_VALUE min, XINPUTW_VALUE max) {
+static XINPUTW_VALUE RangeFindCenter(XINPUTW_VALUE min, XINPUTW_VALUE max)
+{
     /* This is a heuristic, but it should work most of the time */
     if(min == (-max) - 1 || min == -max) return 0;
     else return (XINPUTW_VALUE)((INT32)min + ((INT32)max + (INT32)min) / 2);
 }
 
 static XINPUTW_VALUE ConvToRange(XINPUTW_VALUE value, XINPUTW_VALUE source_min, XINPUTW_VALUE source_max,
-                                 XINPUTW_VALUE target_min, XINPUTW_VALUE target_max) {
+                                 XINPUTW_VALUE target_min, XINPUTW_VALUE target_max)
+{
     INT32 value_internal, source_range, source_center, target_range, target_center;
     BOOL below_center;
 
@@ -51,9 +53,12 @@ static XINPUTW_VALUE ConvToRange(XINPUTW_VALUE value, XINPUTW_VALUE source_min, 
     source_range = below_center ? (INT32)source_center - (INT32)source_min : (INT32)source_max - (INT32)source_center;
     target_range = below_center ? (INT32)target_center - (INT32)target_min : (INT32)target_max - (INT32)target_center;
 
-    if (source_range == 0 || target_range == 0) {
+    if (source_range == 0 || target_range == 0)
+    {
         value_internal = target_center;
-    } else {
+    }
+    else
+    {
         /* scale and shift the value */
         value_internal = (value_internal * target_range) / source_range;
         value_internal = below_center ? target_center - value_internal : target_center + value_internal;
@@ -63,14 +68,16 @@ static XINPUTW_VALUE ConvToRange(XINPUTW_VALUE value, XINPUTW_VALUE source_min, 
     return (XINPUTW_VALUE)value_internal;
 }
 
-static BYTE GetRangeBitCount(XINPUTW_VALUE min, XINPUTW_VALUE max) {
+static BYTE GetRangeBitCount(XINPUTW_VALUE min, XINPUTW_VALUE max)
+{
     BYTE result;
     WORD range;
 
     result = 0;
     range = (WORD)((INT32)max - (INT32)min);
 
-    while (range != 0) {
+    while (range != 0)
+    {
         ++result;
         range = range >> 1;
     }
@@ -79,23 +86,30 @@ static BYTE GetRangeBitCount(XINPUTW_VALUE min, XINPUTW_VALUE max) {
 }
 
 
-XINPUTW_VALUE xiw_util_ConvToXIWValue(XINPUTW_VALUE value, XINPUTW_VALUE range_min, XINPUTW_VALUE range_max) {
+XINPUTW_VALUE xiw_util_ConvToXIWValue(XINPUTW_VALUE value, XINPUTW_VALUE range_min, XINPUTW_VALUE range_max)
+{
     return ConvToRange(value, range_min, range_max, XINPUTW_VAL_MIN, XINPUTW_VAL_MAX);
 }
 
-XINPUTW_VALUE xiw_util_ConvFromXIWValue(XINPUTW_VALUE value, XINPUTW_VALUE range_min, XINPUTW_VALUE range_max) {
+XINPUTW_VALUE xiw_util_ConvFromXIWValue(XINPUTW_VALUE value, XINPUTW_VALUE range_min, XINPUTW_VALUE range_max)
+{
     return ConvToRange(value, XINPUTW_VAL_MIN, XINPUTW_VAL_MAX, range_min, range_max);
 }
 
-void xiw_util_SetCapabilitiesBtn(WORD *buttons, XINPUTW_EVENT_CODE code, BOOL value) {
+void xiw_util_SetCapabilitiesBtn(WORD *buttons, XINPUTW_EVENT_CODE code, BOOL value)
+{
     WORD mask;
 
-    if (code < WINE_BTN_A || code > WINE_BTN_DPAD_RIGHT) return;
+    if (code < WINE_BTN_A || code > WINE_BTN_DPAD_RIGHT)
+    {
+        return;
+    }
     mask = 1ul << code;
     *buttons = (*buttons & ~mask) | (value ? mask : 0);
 }
 
-void xiw_util_SetCapabilitiesAxis(BYTE *axes, XINPUTW_EVENT_CODE code, XINPUTW_VALUE min, XINPUTW_VALUE max) {
+void xiw_util_SetCapabilitiesAxis(BYTE *axes, XINPUTW_EVENT_CODE code, XINPUTW_VALUE min, XINPUTW_VALUE max)
+{
     if (code < WINE_AXIS_LTRIGGER || code > WINE_AXIS_RTHUMB_Y) return;
     axes[code - WINE_AXIS_LTRIGGER] = GetRangeBitCount(min, max);
 }
@@ -104,7 +118,8 @@ void xiw_util_SetCapabilitiesAxis(BYTE *axes, XINPUTW_EVENT_CODE code, XINPUTW_V
 /*
  * Taken and adapted from dlls/dinput/device.c
  */
-BOOL xiw_util_OpenCfgKeys(HKEY *defkey, HKEY *appkey, const char *subkey_path) {
+BOOL xiw_util_OpenCfgKeys(HKEY *defkey, HKEY *appkey, const char *subkey_path)
+{
     char buffer[MAX_PATH+16];
     DWORD len;
 
@@ -112,38 +127,58 @@ BOOL xiw_util_OpenCfgKeys(HKEY *defkey, HKEY *appkey, const char *subkey_path) {
 
     /* Wine registry key: HKCU\Software\Wine\XInput */
     strcpy(buffer, "Software\\Wine\\XInput");
-    if (subkey_path != NULL) {
-        if (subkey_path[0] == '\\') {
+    if (subkey_path != NULL)
+    {
+        if (subkey_path[0] == '\\')
+        {
             strcat(buffer, subkey_path);
-        } else {
+        }
+        else
+        {
             strcat(buffer, "\\");
             strcat(buffer, subkey_path);
         }
     }
     if (RegOpenKeyA(HKEY_CURRENT_USER, buffer, defkey) != ERROR_SUCCESS)
+    {
         *defkey = 0;
+    }
 
     len = GetModuleFileNameA(0, buffer, MAX_PATH);
-    if (len && len < MAX_PATH) {
+    if (len && len < MAX_PATH)
+    {
         HKEY tmpkey;
 
         /* Wine registry key: HKCU\Software\Wine\AppDefaults\app.exe\XInput */
-        if (RegOpenKeyA(HKEY_CURRENT_USER, "Software\\Wine\\AppDefaults", &tmpkey) == ERROR_SUCCESS) {
+        if (RegOpenKeyA(HKEY_CURRENT_USER, "Software\\Wine\\AppDefaults", &tmpkey) == ERROR_SUCCESS)
+        {
             char *p, *appname = buffer;
-            if ((p = strrchr(appname, '/'))) appname = p + 1;
-            if ((p = strrchr(appname, '\\'))) appname = p + 1;
+            if ((p = strrchr(appname, '/')))
+            {
+                appname = p + 1;
+            }
+            if ((p = strrchr(appname, '\\')))
+            {
+                appname = p + 1;
+            }
             strcat(appname, "\\XInput");
-            if (subkey_path != NULL) {
-                if (subkey_path[0] == '\\') {
+            if (subkey_path != NULL)
+            {
+                if (subkey_path[0] == '\\')
+                {
                     strcat(buffer, subkey_path);
-                } else {
+                }
+                else
+                {
                     strcat(buffer, "\\");
                     strcat(buffer, subkey_path);
                 }
             }
 
             if (RegOpenKeyA(tmpkey, appname, appkey) != ERROR_SUCCESS)
+            {
                 *appkey = 0;
+            }
             RegCloseKey(tmpkey);
         }
     }
@@ -154,29 +189,38 @@ BOOL xiw_util_OpenCfgKeys(HKEY *defkey, HKEY *appkey, const char *subkey_path) {
 /*
  * Taken and adapted from dlls/dinput/device.c
  */
-LONG xiw_util_GetCfgValueGeneric(HKEY defkey, HKEY appkey, const char *name, BYTE *buffer, DWORD size, DWORD *type) {
+LONG xiw_util_GetCfgValueGeneric(HKEY defkey, HKEY appkey, const char *name, BYTE *buffer, DWORD size, DWORD *type)
+{
     LONG rc;
 
     /* Try to load the app-specific key */
-    if (appkey) {
+    if (appkey)
+    {
         /* If the key was not found, try the default. On any other return code, return that */
         if ((rc = RegQueryValueExA(appkey, name, NULL, type, (LPBYTE)buffer, &size)) != ERROR_FILE_NOT_FOUND)
+        {
             return rc;
+        }
     }
 
     if (defkey)
+    {
         return RegQueryValueExA(defkey, name, NULL, type, (LPBYTE)buffer, &size);
+    }
 
     return ERROR_FILE_NOT_FOUND;
 }
 
-DWORD xiw_util_GetCfgValueDW(HKEY defkey, HKEY appkey, const char *name, DWORD defaultValue) {
+DWORD xiw_util_GetCfgValueDW(HKEY defkey, HKEY appkey, const char *name, DWORD defaultValue)
+{
     DWORD key_type, value;
     LONG rc;
     rc = xiw_util_GetCfgValueGeneric(defkey, appkey, "ValueToButtonEpsilon",
         (BYTE *)&value, sizeof(DWORD), &key_type);
     if (rc == ERROR_SUCCESS && key_type == REG_DWORD)
+    {
         return value;
+    }
 
     return defaultValue;
 }
